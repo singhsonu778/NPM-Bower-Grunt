5d1501baa029e69e56fe6e489c408869a66272cb
Sonu Kumar Singh <singhsonu778@gmail.com>
2016-06-22 23:24:15 +0530

    Add 'css minify' task to minify css file

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var del = require('del');
 var sass = require('gulp-sass');
 var concatCss = require('gulp-concat-css');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 
 gulp.task('default', function() {
 	console.log('Sonu Singh');
@@ -21,4 +23,11 @@ gulp.task('concatCss', function() {
 	return gulp.src('target/css/**/*.css')
 		   .pipe(concatCss('main/main.css'))
            .pipe(gulp.dest('target/css/'));
+});
+
+gulp.task('cssmin', function () {
+	gulp.src('target/css/main/main.css')
+		.pipe(cssmin())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('target/css/main/'));
 });
