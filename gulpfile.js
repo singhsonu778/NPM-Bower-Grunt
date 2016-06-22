dd73b56ab2ed8961d39fa9c8e6cb1c526eff2109
Sonu Kumar Singh <singhsonu778@gmail.com>
2016-06-22 20:20:28 +0530

    Add 'clean' task to delete generated 'build' and 'target' folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
+var del = require('del');
 
 gulp.task('default', function() {
 	console.log('Sonu Singh');
+});
+
+gulp.task('clean', function() {
+	return del.sync(['build', 'target']);
 });
