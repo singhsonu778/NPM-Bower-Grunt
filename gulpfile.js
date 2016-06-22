b22c6888f26fe119fccf7750c99915935b54ffba
Sonu Kumar Singh <singhsonu778@gmail.com>
2016-06-22 22:58:10 +0530

    Add 'concatCss' task to combine css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var del = require('del');
 var sass = require('gulp-sass');
+var concatCss = require('gulp-concat-css');
 
 gulp.task('default', function() {
 	console.log('Sonu Singh');
@@ -14,4 +15,10 @@ gulp.task('sass', function(){
 	return gulp.src('src/scss/**/*.scss')
 	       .pipe(sass())
            .pipe(gulp.dest('target/css'));
+});
+
+gulp.task('concatCss', function() {
+	return gulp.src('target/css/**/*.css')
+		   .pipe(concatCss('main/main.css'))
+           .pipe(gulp.dest('target/css/'));
 });
