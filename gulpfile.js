01d86fb0b4a2ffc8247386ce8e33d6039b7e4079
Sonu Kumar Singh <singhsonu778@gmail.com>
2016-06-23 23:52:06 +0530

    Refactor code

--- gulpfile.js
+++ gulpfile.js
@@ -11,54 +11,54 @@ var runSequence = require('run-sequence');
 var watch = require('gulp-watch');
 
 gulp.task('default', function(callback) {
-	runSequence('clean', ['css_tasks', 'js_tasks'], 'copy_tasks', 'karma', 'watch', callback);
+	runSequence('clean', ['css', 'js', 'copy'], 'karma', 'watch', callback);
 });
 
 gulp.task('clean', function() {
-	return del.sync(['build', 'target']);
+	return del.sync(['target']);
 });
 
-gulp.task('css_tasks', function(){
+gulp.task('css', function() {
 	return gulp.src('src/scss/**/*.scss')
 	       .pipe(sass())
 	       .pipe(concatCss('main.css'))
 	       .pipe(cssmin())
 	       .pipe(rename({suffix: '.min'}))
-           .pipe(gulp.dest('build'));
+           .pipe(gulp.dest('target'));
 });
 
-gulp.task('js_tasks', function() {
+gulp.task('js', function() {
 	return gulp.src('src/js/*.js')
            .pipe(concat('main.js'))
            .pipe(uglify())
            .pipe(rename({suffix: '.min'}))
-           .pipe(gulp.dest('build'));
+           .pipe(gulp.dest('target'));
 });
 
-gulp.task('copy_tasks', function(callback) {
+gulp.task('copy', function(callback) {
 	runSequence(['copyHtml', 'copyThirdParty'], callback);
 });
 
 gulp.task('copyHtml', function() {
 	return gulp.src('src/index.html')
-           .pipe(gulp.dest('build'));
+           .pipe(gulp.dest('target'));
 });
 
 gulp.task('copyThirdParty', function() {
 	return gulp.src(['bower_components/bootstrap/dist/css/bootstrap.min.css', 'bower_components/jquery/dist/jquery.min.js'])
-           .pipe(gulp.dest('build/third_party'));
+           .pipe(gulp.dest('target/third_party'));
 });
 
-gulp.task('karma', function (done) {
+gulp.task('karma', function(done) {
 	new server({configFile: __dirname + '/karma.conf.js'}, done).start();
 });
 
-gulp.task('watch', function (callback) {
+gulp.task('watch', function(callback) {
 	gulp.watch('src/index.html', ['copyHtml']);
-	gulp.watch('src/scss/*.scss', ['css_tasks']);
+	gulp.watch('src/scss/*.scss', ['css']);
 	gulp.watch('src/js/*.js', ['watchJs']);
 });
 
-gulp.task('watchJs', function (callback) {
-	runSequence('js_tasks', 'karma', callback);
+gulp.task('watchJs', function(callback) {
+	runSequence('js', 'karma', callback);
 });
